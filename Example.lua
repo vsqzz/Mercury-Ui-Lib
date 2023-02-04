a705d98604490daad92f5be5ec076169caabd032
shezan <97002070+shezan78@users.noreply.github.com>
2023-02-03 22:10:03 -0500

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,88 +1,45 @@
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/Mercury-Ui-Lib/main/Source.lua"))()
+local Mercury = loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/Mercury-Ui-Lib/main/Source.lua"))()
 
-local gui = Library:create{
-    Theme = Library.Themes.Serika
+local GUI = Mercury:Create{
+    Name = "Mercury",
+    Size = UDim2.fromOffset(600, 400),
+    Theme = Mercury.Themes.Dark,
+    Link = "https://github.com/shezan78/Mercury-Ui-Lib"
 }
 
-local tab = gui:tab{
-    Icon = "rbxassetid://6034996695",
-    Name = "Aimbot"
+local Tab = GUI:Tab{
+	Name = "Da hood",
+	Icon = "rbxassetid://8569322835"
 }
 
-tab:button({
-    Name = "Aimbot",
-    Callback = function()
-        tab:prompt{
-            Title = "baby",
-            Text = "shark doo doo doo doo im blank lmao",
-            Buttons = {
-                Ok = function()
-                    tab:prompt{
-                        Followup = true,
-                        Title = "really?",
-                        Text = "you sure?=",
-                        Buttons = {
-                            Yes = function()
-                                tab:prompt{
-                                    Followup = true,
-                                    Title = "xd",
-                                    Text = "sus",
-                                    Buttons = {
-                                        dippitydippity = function()
-                                            gui:set_status("github")
-                                        end,
-                                        skipskip = function()
-                                            gui:set_Status("money")
-                                        end
-                                    }
-                                }
-                            end,
-                        }
-                    }
-                end,
-            }
-        }
-    end,
-})
-tab:keybind({Callback = function()
-    gui:prompt()
-end,})
-tab:dropdown({
-    Name = "Dropdown",
-    Description = "Example Txt",
-    StartingText = "Bodypart",
-    Items = {
-        "Head",
-        "Torso",
-        "Random"
-    }
-})
-tab:dropdown({
-    Name = "Example",
-    StartingText = "Number",
-    Items = {
-        {"One", 1},
-        {"Two", 2},
-        {"Three", 3}
-    },
-    Description = "Number Picker",
-    Callback = function(v)
-        print(v, "clicked")
-    end,
-})
-local cum = tab:slider({Callback = function(v)
-    gui:set_status(v)
-end})
+Tab:Button{
+	Name = "Silent Aim",
+	Description = nil,
+	Callback = function() 
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/STREAMABLE_1.lua", true))()
+	    end,
+}
 
-tab:textbox({Callback = function(v)
-    gui:prompt{Text = v}
-end,})
+Tab:Button{
+	Name = "Camlock",
+	Description = nil,
+	Callback = function() 
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/SmoothLock.lua", true))()
+	    end,
+}
 
-tab:color_picker({
-    Name = "your mom's color",
-    Style = Library.ColorPickerStyles.Legacy,
-    Description = "Click to adjust color...",
-    Callback = function(color)
-        print(color)
-    end,
-})
+Tab:Button{
+	Name = "Pink dot lock = Q",
+	Description = nil,
+	Callback = function() 
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/Pink%20Dot%20lock%201.lua", true))()
+	    end,
+}
+
+Tab:Button{
+	Name = "Hat gui",
+	Description = nil,
+	Callback = function() 
+	    loadstring(game:HttpGet("https://raw.githubusercontent.com/shezan78/ALotOfRobloxScripts/main/Roblox%20Scripts/Hat%20Gui.lua", true))()
+	    end,
+}
